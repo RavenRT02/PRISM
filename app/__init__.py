9b12ff7e4162e5388fd01b25d3e02b10659f8cb2
RavenRT02 <praveenkumart261@gmail.com>
2026-03-11 14:33:42 +0530

    SQLAlchemy and SQLMigrate setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from app.auth.routes import auth_bp 
+from app.extensions import db,migrate
 
 def create_app():
 
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")     # Load configuration (settings for app) from the config file -> Config class
+    app.config.from_object("app.config.Config")     # Use class directly instead of string method, Load configuration (settings for app) from the config file -> Config class
+                                                    # Replaced app.config.from_object("app.config.Config") with app.config.from_object(Config)
+
+    db.init_app(app)          # Attach db to flask app
+    migrate.init(app , db)    # Attach migrate to flask app and connect with db         
+                             
     app.register_blueprint(auth_bp)  # register / attach auth blueprint routes to main flask file
 
     return app
