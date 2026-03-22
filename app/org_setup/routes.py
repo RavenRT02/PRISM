6558c7c0bd4421422015ec762de391eb4aae1902
RavenRT02 <praveenkumart261@gmail.com>
2026-03-22 13:12:06 +0530

    Added buildings, floors and rooms table and placeholder html templates to test data

--- app/org_setup/routes.py
+++ app/org_setup/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, redirect, url_for, render_template, request, flash
 from flask_login import login_required, current_user
 from app.extensions import db
-from app.models import Building
+from app.models import Building, Floor, Room
 
 org_bp = Blueprint("org_setup", __name__)
 
@@ -32,8 +32,62 @@ def add_building():
         db.session.add(building)
         db.session.commit()
 
-        flash(f'{name} added Successfully to buildings', "Success")
+        flash(f'{name} added Successfully to buildings', "success")
 
     buildings = Building.query.all()
     
     return render_template("add_building.html", buildings = buildings)
+
+
+@org_bp.route("/add-floor", methods = ['GET', 'POST'])
+@login_required
+@admin_required
+def add_floor():
+
+    buildings = Building.query.filter_by(is_active = True).all()
+
+    if request.method == 'POST':
+
+        number = request.form.get("number")
+        building_id = request.form.get('building_id')
+        is_active = request.form.get("is_active") == "on"
+
+        floor = Floor( number = number, building_id = building_id, is_active = is_active)
+
+        db.session.add(floor)
+        db.session.commit()
+
+        building = Building.query.get(building_id)
+
+        flash(f'Floor {number} added to {building.name} Successfully', "success")
+
+    floors = Floor.query.all()
+
+    return render_template("add_floor.html", buildings = buildings, floors = floors)
+
+
+@org_bp.route("/add-room", methods = ['GET', 'POST'])
+@login_required
+@admin_required
+def add_room():
+
+    floors = Floor.query.filter_by(is_active = True).all()
+
+    if request.method == 'POST':
+
+        name = request.form.get("name")
+        floor_id = request.form.get("floor_id")
+        is_active = request.form.get("is_active") == "on"
+
+        room = Room( name = name, floor_id = floor_id, is_active = is_active)
+
+        db.session.add(room)
+        db.session.commit()
+
+        floor = Floor.query.get(floor_id)
+
+        flash(f'Room {name} added to floor {floor.number} Successfully', "success")
+
+    rooms = Room.query.all()
+
+    return render_template("add_room.html", floors = floors, rooms = rooms)
